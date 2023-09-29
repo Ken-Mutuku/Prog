76bcb62a040a2b7a3be4ce9cc06646671667ee54
Ken-Mutuku <146414376+Ken-Mutuku@users.noreply.github.com>
2023-09-29 10:34:05 +0300

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -2,6 +2,7 @@
 username = input("Enter name")
 fees = int(input("Enter your the fee that you pay per semester"))
 courseduration = int(input(" Enter the duration that your course is to take"))
-Totalfees = (fees*courseduration*2)
-Statement = username+", the total amount of fees that you will require for this course is {} in {} years."
+tution= 15000
+Totalfees = (fees*courseduration*2)+ (tution*4)
+Statement = username+", the total amount of fees that you will require for this course is {} in {} years,inclusive of tution."
 print (Statement.format(Totalfees, courseduration))
