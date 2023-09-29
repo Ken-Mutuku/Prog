b8eaa6cb66d3c92a8a841b0e8fa596f9cce22ea6
Ken-Mutuku <146414376+Ken-Mutuku@users.noreply.github.com>
2023-09-29 10:08:58 +0300

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,7 @@
+#SCT211-0050/2022 KEN MUTUKU
+username = input("Enter name")
+fees = int(input("Enter your the fee that you pay per semester"))
+courseduration = int(input(" Enter the duration that your course is to take"))
+Totalfees = (fees*courseduration*2)
+Statement = username+", the total amount of fees that you will require for this course is {} in {} years."
+print (Statement.format(Totalfees, courseduration))
